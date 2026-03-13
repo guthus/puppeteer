836c5aa020538649e0ec28b2ad5ffb35fa2c295c
guthus <falecom@augusthus.com.br>
2026-03-13 19:21:06 -0300

    fix syntax error

--- index.js
+++ index.js
@@ -8,35 +8,36 @@ app.get("/", (req, res) => {
 });
 
 app.get("/os", async (req, res) => {
+
   try {
 
     const response = await axios.post(
       "https://www.bling.com.br/services/ordem.servicos.server.php?f=listarOrdensServicos",
       "xajax=%23Services%5COrdemServico%5C%23listarOrdensServicos&xajaxr=1",
       {
-	headers: {
-  "content-type": "application/x-www-form-urlencoded",
-  cookie: `PHPSESSID=${process.env.PHPSESSID}; PCSID=${process.env.PCSID}`
-}
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          cookie: `PHPSESSID=${process.env.PHPSESSID}; PCSID=${process.env.PCSID}`
         }
       }
     );
 
-    const data = response.data;
-
-    res.json(data);
+    res.json(response.data);
 
   } catch (err) {
+
+    console.error(err);
 
     res.status(500).json({
       erro: err.message
     });
 
   }
+
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("running", PORT);
+  console.log("Server running on port", PORT);
 });
