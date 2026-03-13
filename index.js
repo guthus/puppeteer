873cd5768a99df926a96e99af4ab85178efb6f93
guthus <falecom@augusthus.com.br>
2026-03-13 18:52:03 -0300

    fix bling oauth login5

--- index.js
+++ index.js
@@ -1,99 +1,42 @@
 const express = require("express");
-const { chromium } = require("playwright");
+const axios = require("axios");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ status: "API online" });
+  res.json({ status: "online" });
 });
 
 app.get("/os", async (req, res) => {
-  let browser;
-
   try {
-    console.log("Launching browser...");
 
-    browser = await chromium.launch({
-      headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu"
-      ]
-    });
-
-    const page = await browser.newPage();
-
-console.log("Opening login page...");
-
-await page.goto("https://www.bling.com.br/Api/v3/oauth/login?env=production", {
-  waitUntil: "domcontentloaded",
-  timeout: 60000
-});
-
-console.log("Waiting login fields...");
-
-await page.waitForSelector('input[name="login"]', { timeout: 60000 });
-
-console.log("Typing credentials...");
-
-await page.fill('input[name="login"]', process.env.BLING_USER);
-await page.fill('input[name="password"]', process.env.BLING_PASS);
-
-console.log("Submitting login...");
-
-await Promise.all([
-  page.click("button[type=submit]"),
-  page.waitForNavigation({ waitUntil: "domcontentloaded" })
-]);
-
-    console.log("Going to OS page...");
-
-    await page.goto("https://www.bling.com.br/ordem.servicos.php", {
-      waitUntil: "domcontentloaded",
-      timeout: 60000
-    });
-
-    await page.waitForTimeout(4000);
-
-    console.log("Extracting OS...");
-
-    const os = await page.evaluate(() => {
-      const linha = document.querySelector("table tbody tr");
-
-      if (!linha) {
-        return { erro: "Nenhuma OS encontrada" };
+    const response = await axios.post(
+      "https://www.bling.com.br/services/ordem.servicos.server.php?f=listarOrdensServicos",
+      "xajax=%23Services%5COrdemServico%5C%23listarOrdensServicos&xajaxr=1",
+      {
+	headers: {
+  "content-type": "application/x-www-form-urlencoded",
+  cookie: `PHPSESSID=${process.env.PHPSESSID}; PCSID=${process.env.PCSID}`
+}
+        }
       }
+    );
 
-      const colunas = linha.querySelectorAll("td");
+    const data = response.data;
 
-      return {
-        numero: colunas[1].innerText.trim(),
-        cliente: colunas[3].innerText.trim()
-      };
-    });
-
-    await browser.close();
-
-    res.json(os);
+    res.json(data);
 
   } catch (err) {
-
-    console.error("Error:", err);
-
-    if (browser) {
-      await browser.close();
-    }
 
     res.status(500).json({
       erro: err.message
     });
+
   }
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
+  console.log("running", PORT);
 });
