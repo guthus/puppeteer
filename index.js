1cf15ede24f8a6a0554394f363745122aee6614f
guthus <falecom@augusthus.com.br>
2026-03-13 16:39:12 -0300

    Fix Chromium path2

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ const browser = await puppeteer.launch({
   args: ["--no-sandbox", "--disable-setuid-sandbox"]
 });
     const page = await browser.newPage();
-    await page.goto("https://www.bling.com.br/login");
-    await page.type("#login", process.env.BLING_USER);
-    await page.type("#password", process.env.BLING_PASS);
+    await page.goto("https://www.bling.com.br/login", {waitUntil:"networkidle2"});
+
+    await page.waitForSelector('input[name="login"]');
+
+    await page.type('input[name="login"]', process.env.BLING_USER);
+    await page.type('input[name="password"]', process.env.BLING_PASS);
+
     await page.click("button[type=submit]");
-    await page.waitForNavigation();
+
+    await page.waitForNavigation({waitUntil:"networkidle2"});
     await page.goto("https://www.bling.com.br/ordem.servicos.php");
     await page.waitForTimeout(3000);
     const os = await page.evaluate(() => {
