6fe2979f044e89122d56f67fb37276c081c7db44
guthus <falecom@augusthus.com.br>
2026-03-13 17:23:31 -0300

    Fix: listen on 0.0.0.0

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.get("/test", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
