763277e12b5117461925e29b517d6d504e035344
guthus <falecom@augusthus.com.br>
2026-03-13 16:12:39 -0300

    Fix: Add Procfile and optimize Puppeteer startup

--- index.js
+++ index.js
@@ -1,51 +1,32 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
-
 const app = express();
 
 app.get("/os", async (req, res) => {
-
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-  });
-
-  const page = await browser.newPage();
-
-  await page.goto("https://www.bling.com.br/login");
-
-  await page.type("input[name=login]", process.env.BLING_USER);
-  await page.type("input[name=password]", process.env.BLING_PASS);
-
-  await page.click("button[type=submit]");
-
-  await page.waitForNavigation();
-
-  await page.goto("https://www.bling.com.br/ordem.servicos.php");
-
-  await page.waitForTimeout(4000);
-
-  const os = await page.evaluate(() => {
-
-    const linha = document.querySelector("table tbody tr");
-
-    const numero = linha.querySelectorAll("td")[1].innerText;
-    const cliente = linha.querySelectorAll("td")[3].innerText;
-
-    return {
-      numero,
-      cliente
-    };
-
-  });
-
-  await browser.close();
-
-  res.json(os);
-
+  try {
+    const browser = await puppeteer.launch({
+      executablePath: "/usr/bin/chromium-browser",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.bling.com.br/login");
+    await page.type("#login", process.env.BLING_USER);
+    await page.type("#password", process.env.BLING_PASS);
+    await page.click("button[type=submit]");
+    await page.waitForNavigation();
+    await page.goto("https://www.bling.com.br/ordem.servicos.php");
+    await page.waitForTimeout(3000);
+    const os = await page.evaluate(() => {
+      const linha = document.querySelector("table tbody tr");
+      const numero = linha.querySelector("td").innerText;
+      const cliente = linha.querySelectorAll("td")[3].innerText;
+      return { numero, cliente };
+    });
+    await browser.close();
+    res.json(os);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running");
-});
+app.listen(3000, () => console.log("running on port 3000"));
