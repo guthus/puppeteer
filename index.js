8c8fc006d57b6683ebb4f48ac0f960e8d32080fb
guthus <falecom@augusthus.com.br>
2026-03-13 21:53:15 -0300

    BLING COOKIE

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.get("/os", async (req, res) => {
       "https://www.bling.com.br/services/ordem.servicos.server.php?f=listarOrdensServicos",
       "xajax=%23Services%5COrdemServico%5C%23listarOrdensServicos&xajaxr=1",
       {
-headers: {
-  "content-type": "application/x-www-form-urlencoded",
-  "user-agent": "Mozilla/5.0",
-  "origin": "https://www.bling.com.br",
-  "referer": "https://www.bling.com.br/ordem.servicos.php",
-  "cookie": `PHPSESSID=${process.env.PHPSESSID}; PCSID=${process.env.PCSID}`
-}
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "user-agent": "Mozilla/5.0",
+          "origin": "https://www.bling.com.br",
+          "referer": "https://www.bling.com.br/ordem.servicos.php",
+          "cookie": process.env.BLING_COOKIE
+        }
       }
     );
 
